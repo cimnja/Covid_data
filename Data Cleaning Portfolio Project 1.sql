68aabe927c45ad3eb8522db59dfb52fe6fb027ac
Anja Cimerman <103211671+cimnja@users.noreply.github.com>
2022-07-05 21:42:45 +0200

    Update Data Cleaning Portfolio Project 1.sql

--- Data Cleaning Portfolio Project 1.sql
+++ Data Cleaning Portfolio Project 1.sql
@@ -4,73 +4,73 @@ WHERE continent is not null
 ORDER BY 3, 4
 
 
---Select *
---FROM PortfolioProject..CovidVaccinations
---ORDER BY 3, 4
 
-
-SELECT LOCATION, DATE, TOTAL_CASES, NEW_CASES, TOTAL_DEATHS, POPULATION 
+SELECT location, date, total_cases, new_cases, total_deaths, population 
 FROM PortfolioProject..CovidDeaths
+WHERE continet is not null
 ORDER BY 1, 2
 
---Total cases vs Total Deaths 
+--Total cases vs Total Deaths (likelihood of death if infected in a specific country)
 
-SELECT LOCATION, DATE, TOTAL_CASES, TOTAL_DEATHS, (total_deaths/total_cases)*100 as DeathPercentage
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 FROM PortfolioProject..CovidDeaths
-WHERE LOCATION LIKE '%DENMA%'
-ORDER BY 1, 2
-
-
---Total cases vs population 
-
-SELECT LOCATION, DATE, population, TOTAL_CASES, (total_cases/population)*100 as InfectedPercentage 
-FROM PortfolioProject..CovidDeaths
---WHERE LOCATION LIKE '%DENMA%'
+WHERE location LIKE '%denma%'
+AND continent is not null
 ORDER BY 1, 2
 
 
---Highest infection rate 
+--Total cases vs population (what % of population is infected)
 
-SELECT LOCATION, population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 as InfectedPercentage 
+SELECT location, date, population, total_cases, (total_cases/population)*100 as InfectedPercentage 
 FROM PortfolioProject..CovidDeaths
---WHERE LOCATION LIKE '%DENMA%'
-GROUP BY LOCATION, population
+--WHERE location LIKE '%denma%'
+ORDER BY 1, 2
+
+
+--Highest infection rate compated to population
+
+SELECT location, population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 as InfectedPercentage 
+FROM PortfolioProject..CovidDeaths
+--WHERE location LIKE '%denma%'
+GROUP BY location, population
 ORDER BY InfectedPercentage DESC
 
 
 --Highest death count per population 
 
-SELECT LOCATION, population, MAX(CAST(total_deaths as bigint)) AS TotalDeathCount, MAX((total_deaths/population))*100 as DeathPercentage 
+SELECT location, population, MAX(cast(total_deaths as bigint)) AS TotalDeathCount, MAX((total_deaths/population))*100 as DeathPercentage 
 FROM PortfolioProject..CovidDeaths
---WHERE LOCATION LIKE '%DENMA%'
+--WHERE location LIKE '%denma%'
 WHERE continent is not null
-GROUP BY LOCATION, population
+GROUP BY location
 ORDER BY TotalDeathCount DESC
 
 
---By continet 
+--Breaking data down by continet 
 
-SELECT location, MAX(CAST(total_deaths as bigint)) AS TotalDeathCount
+--Continents with highest death count per population
+
+SELECT location, MAX(cast(total_deaths as bigint)) AS TotalDeathCount
 FROM PortfolioProject..CovidDeaths
---WHERE LOCATION LIKE '%DENMA%'
+--WHERE location LIKE '%denma%'
 WHERE continent is null
-GROUP BY location
+GROUP BY continent
 ORDER BY TotalDeathCount DESC
 
 
 
 --Global numbers 
 
-SELECT DATE, SUM(NEW_CASES) as TotalCases, SUM(CAST(new_deaths as bigint)) as 'Total Deaths', 
-			SUM(CAST(new_deaths as bigint))/SUM(new_cases)*100 as DeathPercentage 
+SELECT DATE, SUM(new_cases) as TotalCases, SUM(cast(new_deaths as bigint)) as 'Total Deaths', 
+			SUM(cast(new_deaths as bigint))/SUM(new_cases)*100 as DeathPercentage 
 FROM PortfolioProject..CovidDeaths
---WHERE LOCATION LIKE '%DENMA%'
+--WHERE location LIKE '%denma%'
 WHERE continent is not null 
 GROUP BY date 
 ORDER BY 1, 2
 
 
---Total population vs Vaccinations 
+--Total population vs Vaccinations (% of population that recieved at least one vaccine)
 
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
 		SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (partition by dea.location ORDER BY dea.location, dea.date) as PeopleVaccinated
@@ -95,9 +95,9 @@ SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinatio
 		SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (partition by dea.location ORDER BY dea.location, dea.date) as PeopleVaccinated 
 
 FROM PortfolioProject..CovidDeaths dea
-Join PortfolioProject..CovidVaccinations vac 
-	On dea.location = vac.location
-and		dea.date = vac.date
+JOIN PortfolioProject..CovidVaccinations vac 
+	ON dea.location = vac.location
+AND		dea.date = vac.date
 --WHERE dea.continent is not null 
 WHERE vac.new_vaccinations is not null AND vac.continent is not null 
 )
@@ -121,14 +121,14 @@ FROM PopVac
  PeopleVaccinated numeric
  )
 
- INSERT INTO #PercentPopulationVaccinated
+INSERT INTO #PercentPopulationVaccinated
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
 		SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (partition by dea.location ORDER BY dea.location, dea.date) as PeopleVaccinated 
 
 FROM PortfolioProject..CovidDeaths dea
-Join PortfolioProject..CovidVaccinations vac 
-	On dea.location = vac.location
-and		dea.date = vac.date
+JOIN PortfolioProject..CovidVaccinations vac 
+	ON dea.location = vac.location
+AND		dea.date = vac.date
 --WHERE dea.continent is not null 
 --WHERE vac.new_vaccinations is not null AND vac.continent is not null 
 
@@ -143,9 +143,9 @@ SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinatio
 		SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (partition by dea.location ORDER BY dea.location, dea.date) as PeopleVaccinated 
 
 FROM PortfolioProject..CovidDeaths dea
-Join PortfolioProject..CovidVaccinations vac 
-	On dea.location = vac.location
-and		dea.date = vac.date
+JOIN PortfolioProject..CovidVaccinations vac 
+	ON dea.location = vac.location
+AND		dea.date = vac.date
 WHERE vac.new_vaccinations is not null AND vac.continent is not null 
 --ORDER BY 2, 3
 
